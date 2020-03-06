3a5d00285fdb702fa036f8269a77677e9de697ed
Mohammad Nurdin (鳴持无) <mohammadnrdn@gmail.com>
2020-03-06 11:28:07 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,72 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+ header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+ header('Content-Type: application/json');
+/*
+ * Following code will get single task details
+ * A task is identified by task id (taskId)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+
+$postdata = json_decode(file_get_contents("php://input"), true);
+$email = $postdata['email'];
+$password = $postdata['password'];
+ 
+// check for post data
+if (isset($email) && isset($password)) {
+ 
+    // get a task from task table
+    $result = mysqli_query($db->connect(),"SELECT *FROM users WHERE email = '$email' AND password = '$password'");
+ 
+    if (!empty($result)) {
+        // check for empty result
+        if (mysqli_num_rows($result) > 0) {
+ 
+            $result = mysqli_fetch_array($result);
+ 
+            $user = array();
+            $user["userId"] = $result["userId"];
+            // success
+            $response["success"] = 1;
+ 
+            // user node
+            $response["user"] = array();
+ 
+            array_push($response["user"], $user);
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no task found
+            $response["success"] = 0;
+            $response["message"] = "Login failed";
+ 
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no task found
+        $response["success"] = 0;
+        $response["message"] = "Login failed";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
